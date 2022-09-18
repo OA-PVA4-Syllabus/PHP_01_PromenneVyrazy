85608a9f5f592fea4166d4661d9e7ea900414a51
Adam Fišer <adam.fiser@wanex.cz>
2022-09-18 16:46:19 +0200

    Update 3_spojovaniRetezcu.php
    rozšíření zadání skládání textu

--- Cviceni_01_Promenne_Vystupy/3_spojovaniRetezcu.php
+++ Cviceni_01_Promenne_Vystupy/3_spojovaniRetezcu.php
@@ -4,3 +4,10 @@
 /*
  * Vytvořte skript, který bude mít deklarovanou proměnnou jméno, příjmení a jako výstup pomocí skládání řetězců zobrazí `Jmenuji se jméno příjmení a studuji informatiku.`
  * */
+
+
+/*
+ * Sestavte skript pro očekávaný výstup "Vítejte na obsahu webu [subjekt]", kde místo subjekt bude uvedeno jméno libovolné společnosti/stránky.
+ * Vyzkoušejte na třech příkladech. 
+ * */
+
