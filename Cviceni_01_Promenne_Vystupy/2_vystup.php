ce198c9b0ff384a22ae1d9d5075a549293423611
Adam Fišer <adam.fiser@wanex.cz>
2022-09-18 17:02:01 +0200

    Update 2_vystup.php
    rozšíření zadání

--- Cviceni_01_Promenne_Vystupy/2_vystup.php
+++ Cviceni_01_Promenne_Vystupy/2_vystup.php
@@ -6,3 +6,15 @@
  * Pro proměnné A a B udělejte script, který provede matematickou operaci sčítání, násobení a každý výsledek uloží do samostatné proměnné.
  * Každý výsledek zobrazte na samostatném řádku.
  */
+
+/*
+ * Deklarujte:
+ * Proměnná alfa obsahuje hodnotu 200
+ * Proměnná beta obsahuje hodnotu 350
+ * Proměnná gama obsahuje hodnotu 100
+ *
+ * Vhodným způsobem zaměňte obsah hodnot
+ * Proměnná alfa obsahuje hodnotu beta
+ * Proměnná beta obsahuje hodnotu alfa
+ * Proměnná gama obsahuje hodnotu beta plus gama
+ * */
